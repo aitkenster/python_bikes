a12d97af1cdf912929d51f7866395ca3c054d421
Nicola Aitken <naitken22@gmail.com>
2014-10-08 13:42:12 +0100

    checks if an item is an instance of a bike

--- dockingstation.py
+++ dockingstation.py
@@ -1,4 +1,22 @@
+from bike import *
+
 class DockingStation:
 		def __init__(self):
 			  self.capacity = 10
 			  self.bikes = []
+
+		def dock(self, item):
+				if len(self.bikes) < self.capacity:
+						self.bikes.append(item)
+				else:
+						return 'Bike station full'
+
+		def undock(self, item):
+				self.bikes.remove(item)
+
+		def check_item(self, item):
+				if item.isinstance(Bike):
+						return True
+				else:
+						return False
+
--- test_dockingstation.py
+++ test_dockingstation.py
@@ -1,13 +1,50 @@
+import pytest
+from ludibrio import *
 from dockingstation import *
+
+class Chicken:
+		 def squawk():
+		 		return "squawk"
 
 class TestDockingStation:
 
 		def setup_method(self, method):
 				self.bank = DockingStation()
+				with Mock() as self.chicken:
+						self.chicken = Chicken()
+				with Mock() as self.bike:
+						self.bike = Bike()
 
+		def fill_station(self, station):
+				for i in range(0,station.capacity):
+						station.dock(self.bike)
 
 		def test_initialized_with_default_capacity(self):
 				assert self.bank.capacity == 10
 
 		def test_no_bikes_when_created(self):
 				assert len(self.bank.bikes) == 0
+
+		def test_can_dock_a_bike(self):
+				self.bank.dock(self.bike)
+				assert len(self.bank.bikes) == 1
+
+		def test_can_undock_a_bike(self):
+			  self.bank.dock(self.bike)
+			  self.bank.undock(self.bike)
+			  assert len(self.bank.bikes) == 0
+
+		def test_knows_item_is_not_a_bike(self):
+				with Stub() as self.chicken:
+						self.chicken.isinstance(Bike) >> False
+				assert self.bank.check_item(self.chicken) == False
+
+		def test_knows_item_is_a_bike(self):
+				with Stub() as self.bike:
+						self.bike.isinstance(Bike) >> True
+				assert self.bank.check_item(self.bike) == True
+
+		def test_cannot_dock_bike_if_station_full(self):
+			  self.fill_station(self.bank)
+			  assert self.bank.dock(self.bike) == 'Bike station full'
+			  assert len(self.bank.bikes) == 10
