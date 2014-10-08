96aa1874767562dc4439d1e92871a9359e6ccdfc
Nicola Aitken <naitken22@gmail.com>
2014-10-08 14:21:29 +0100

    can select all punctured bikes

--- dockingstation.py
+++ dockingstation.py
@@ -4,19 +4,26 @@ class DockingStation:
 		def __init__(self):
 			  self.capacity = 10
 			  self.bikes = []
+			  self.punctured_bikes = []
 
 		def dock(self, item):
-				if len(self.bikes) < self.capacity:
+				if len(self.bikes) < self.capacity and self.check_is_bike(item) == True:
 						self.bikes.append(item)
 				else:
-						return 'Bike station full'
+						return 'Bike station cannot dock right now'
 
 		def undock(self, item):
 				self.bikes.remove(item)
 
-		def check_item(self, item):
+		def check_is_bike(self, item):
 				if item.isinstance(Bike):
 						return True
 				else:
 						return False
+
+		def get_punctured_bikes(self):
+				for bike in self.bikes:
+						if bike.punctured == True:
+								self.punctured_bikes.append(bike)
+								
 
--- test_dockingstation.py
+++ test_dockingstation.py
@@ -2,18 +2,16 @@ import pytest
 from ludibrio import *
 from dockingstation import *
 
-class Chicken:
-		 def squawk():
-		 		return "squawk"
-
 class TestDockingStation:
 
 		def setup_method(self, method):
 				self.bank = DockingStation()
-				with Mock() as self.chicken:
-						self.chicken = Chicken()
-				with Mock() as self.bike:
-						self.bike = Bike()
+				with Stub() as self.bike:
+						self.bike.isinstance(Bike) >> True
+				with Stub() as self.chicken:
+						self.chicken.isinstance(Bike) >> False
+				with Stub() as self.punctured_bike:
+						self.punctured_bike.punctured >> True
 
 		def fill_station(self, station):
 				for i in range(0,station.capacity):
@@ -35,16 +33,17 @@ class TestDockingStation:
 			  assert len(self.bank.bikes) == 0
 
 		def test_knows_item_is_not_a_bike(self):
-				with Stub() as self.chicken:
-						self.chicken.isinstance(Bike) >> False
-				assert self.bank.check_item(self.chicken) == False
+				assert self.bank.check_is_bike(self.chicken) == False
 
 		def test_knows_item_is_a_bike(self):
-				with Stub() as self.bike:
-						self.bike.isinstance(Bike) >> True
-				assert self.bank.check_item(self.bike) == True
+				assert self.bank.check_is_bike(self.bike) == True
 
 		def test_cannot_dock_bike_if_station_full(self):
 			  self.fill_station(self.bank)
-			  assert self.bank.dock(self.bike) == 'Bike station full'
+			  assert self.bank.dock(self.bike) == 'Bike station cannot dock right now'
 			  assert len(self.bank.bikes) == 10
+
+		def test_can_return_punctured_bikes(self):
+				self.bank.bikes.append(self.punctured_bike)
+				self.bank.get_punctured_bikes()
+				assert len(self.bank.punctured_bikes) == 1
