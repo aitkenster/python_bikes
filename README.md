1dee7285df3a25ccc52c9e83cce2eeb230d49803
Nicola Aitken <naitken22@gmail.com>
2014-10-09 14:48:09 +0100

    added readme

---
+++ README.md
@@ -0,0 +1,36 @@
+Python Bikes
+==========
+
+A Python version of the Boris Bikes program
+
+###Technologies used
+
++ Python
++ Pytest
++ Ludibrio
+
+###How to set it up
+
+```sh
+git clone https://github.com/aitkenster/python-bikes.git
+```
+
+###How to run it
+
+```sh
+cd python-bikes
+python
+execfile('bike.py')
+execfile('bikecontainer.py')
+execfile('van.py')
+execfile('garage.py')
+execfile('dockingstation.py')
+
+```
+
+###How to test it
+
+```sh
+cd python-bikes
+py.test
+``` 
--- van.py
+++ van.py
@@ -6,33 +6,25 @@ class Van(BikeContainer):
 
 		def collect_bikes(self, location, status):
 				if status == "punctured":
-						self.get_punctured_bikes(location)
+						self.get_type_bikes(self.punctured_bikes, location.punctured_bikes)
 				elif status == "working":
-						self.get_working_bikes(location)
+						self.get_type_bikes(self.working_bikes, location.working_bikes)
 				else:
 						return "status not recognised"
 
-		def get_punctured_bikes(self, location):
-				while len(location.punctured_bikes) > 0:
-						self.punctured_bikes.append(location.punctured_bikes.pop())
-
-		def get_working_bikes(self, location):
-				while len(location.working_bikes) > 0:
-						self.working_bikes.append(location.working_bikes.pop())
+		def get_type_bikes(self, type, location):
+				while len(location) > 0:
+						type.append(location.pop())
 
 		def deposit_bikes(self, location, status):
 				if status == "punctured":
-						self.deposit_punctured_bikes(location)
+						self.deposit_type_bikes(location.punctured_bikes, self.punctured_bikes)
 				elif status == "working":
-						self.deposit_working_bikes(location)
+						self.deposit_type_bikes(location.working_bikes, self.working_bikes)
 				else:
 						return "status not recognised"
 
-		def deposit_punctured_bikes(self, location):
-				while len(self.punctured_bikes) > 0:
-						location.punctured_bikes.append(self.punctured_bikes.pop())
-
-		def deposit_working_bikes(self, location):
-				while len(self.working_bikes) > 0:
-							location.working_bikes.append(self.working_bikes.pop())
+		def deposit_type_bikes(self, location, type):
+				while len(type) > 0:
+						location.append(type.pop())
 
